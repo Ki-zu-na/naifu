f936cfe0e9c6940e9584d9770a62c8e46c735eb1
Ki-zu-na <www.3438538167@gmail.com>
2024-11-14 22:06:37 +0800

    fix shuffle_prompts

--- data/processors.py
+++ data/processors.py
@@ -53,10 +53,10 @@ def shuffle_prompts_sdstyle(e: Entry):
     
     # New parameters for dropping all tags before or after keep_tokens_separator
     drop_all_fixed_prob = 0.05  # Probability to drop all fixed tokens
-    drop_all_flex_prob = 0.1    # Probability to drop all flex tokens
+    drop_all_flex_prob = 0.2    # Probability to drop all flex tokens
     
     # New parameter for dropping the first tag when there's no keep_tokens_separator
-    drop_first_tag_prob = 0.1   # Probability to drop the first tag when there's no separator
+    drop_first_tag_prob = 0.05   # Probability to drop the first tag when there's no separator
 
     if keep_tokens_separator not in e.prompt:
         # Handle the case when there's no keep_tokens_separator
@@ -88,6 +88,7 @@ def shuffle_prompts_sdstyle(e: Entry):
         flex_tokens = []
     else:
         if shuffle_caption:
+            fixed_tokens = random.shuffle(fixed_tokens)
             random.shuffle(flex_tokens)
         
         # Apply individual token dropout to flex tokens
